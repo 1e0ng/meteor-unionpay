c379f0b17a733b9734ee18cc8f23fb63644bba92
Liang Sun <i@liangsun.org>
2015-10-09 20:50:51 +0800

    Work on demo.

--- unionpay/unionpay.js
+++ unionpay/unionpay.js
@@ -6,7 +6,7 @@ console.log(Meteor.settings.UnionPay.encrypt.PAN);
 
 ///////////////////
 
-var getSignSN = function() {
+function getSignP12() {
   var certPath = Meteor.settings.UnionPay.sign.certPath;
   var password = Meteor.settings.UnionPay.sign.password;
 
@@ -22,18 +22,71 @@ var getSignSN = function() {
   // decrypt p12 using non-strict parsing mode (resolves some ASN.1 parse errors)
   var p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, password);
   //console.log(p12);
+  return p12;
+}
 
+function getSignSN() {
+  var p12 = getSignP12();
   var x509 = p12.getBags({bagType: forge.pki.oids.certBag});
+  //console.log(forge.pki.oids['1.2.840.113549.1.12.10.1.2']);
   x509 = _.values(x509)[0][0];
-  //console.log(x509);
-
   var sn = x509.cert.serialNumber;
-  console.log(sn);
   return sn;
 };
 
-var customerInfo = function() {
-  return '';
+function getSignFunc() {
+  var p12 = getSignP12();
+  //console.log(forge.pki.oids);
+  var bag = p12.getBags({bagType: forge.pki.oids.pkcs8ShroudedKeyBag});
+  var ans = _.values(bag)[0][0].key.sign;
+  return ans;
+}
+
+function customerInfo() {
+  var ans = '{customerNm=全渠道}';
+  ans = new Buffer(ans).toString('base64');
+  return ans;
+}
+
+function accNo() {
+  accNo = '6216261000000000018';
+  return accNo;
+}
+
+function encryptCertId() {
+  var pki = forge.pki;
+  var certPath = Meteor.settings.UnionPay.encrypt.certPath;
+
+  var fs = Npm.require('fs');
+  var pem = fs.readFileSync(certPath);
+  var cert = pki.certificateFromPem(pem);
+  var sn = cert.serialNumber;
+  //console.log(sn);
+  return sn;
+}
+
+function obj2str(params) {
+  var keys = _.keys(params).sort();
+  var ans = '';
+  _.each(keys, function(key) {
+    ans += key + '=' + params[key] + '&';
+  });
+  ans = ans.slice(0, -1);
+  return ans;
+}
+
+function sign(params) {
+  var params_str = obj2str(params);
+  var md = forge.md.sha1.create();
+  md.update(params_str);
+  var sha1 = md.digest().toHex();
+  console.log(sha1);
+
+  var signFunc = getSignFunc();
+  var ans = signFunc(md);
+  signature = new Buffer(ans).toString('base64');
+  console.log(signature);
+  params.signature = signature;
 }
 
 var params = {
@@ -54,20 +107,23 @@ var params = {
   orderId: moment().format('YYYYMMDDHHmmss'),
   txnTime: moment().format('YYYYMMDDHHmmss'),
   accType: '01',
-  accNo: '',
+  accNo: accNo(),
   customerInfo: customerInfo(),
   reqReserved: '',
   reserved: '',
   riskRateInfo: '',
-  encryptCertId: '',
+  encryptCertId: encryptCertId(),
   userMac: '',
-  bindId: '',
   relTxnType: '02',
   payCardType: '01',
-  issInsCode: '',
-  vpcTransData: ''
+  issInsCode: ''
 };
+
+sign(params);
 
 console.log(params);
 
+result = HTTP.post(Meteor.settings.UnionPay.url.backEndRequest, {data: params, headers: {'Content-type': 'application/x-www-form-urlencoded', 'charset': 'UTF-8', timeout: 5000}});
 
+
+console.log(result);
